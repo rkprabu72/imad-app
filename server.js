fedc14dcd7ee4da3087550193fcc4a49fbbac96a
rkprabu72 <30988241+rkprabu72@users.noreply.github.com>
2017-10-01 20:15:31 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/', function (req, res) {
 app.get('/prabhu', function (req, res) {
   res.send('You have called Prabhu');
 });
+app.get('/Sudarshan', function (req, res) {
+  res.send('You have called Sudarshan');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
